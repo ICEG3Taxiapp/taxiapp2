203e2eda8c22b113171ac0c2effd8be60e1273a2
Hanz <Hansikam94@gmail.com>
2016-01-20 23:08:34 +0530

    Driver contact no shown

--- ajax.php
+++ ajax.php
@@ -9,17 +9,20 @@ if (isset($_POST['driverId'])) {
     
     $queryup = mysql_query("UPDATE `taxiapp`.`hire_request` SET `completed` = '1' WHERE `hire_request`.`request_id` = '$requestId'");
     
-    $query = mysql_query("SELECT `driver_id`,`name`,`request_id`,`bid` FROM driverbid NATURAL JOIN driver WHERE request_id = '$requestId' AND driver_id = '$driverId'");
+    $query = mysql_query("SELECT `driver_id`,`name`,`request_id`,`bid`,`contact_no` FROM driverbid NATURAL JOIN driver WHERE request_id = '$requestId' AND driver_id = '$driverId'");
       while($row = mysql_fetch_array($query)){ // display all rows  from query
 		$driverName = $row['name'];
 		$driverId = $row['driver_id'];
 		$bid = $row['bid'];
+        $contact_no = $row['contact_no'];
        
     }
     
     $result = mysql_query("INSERT INTO `taxiapp`.`tour` ( `charge`, `driver_id`, `request_id`) VALUES ( '$bid', '$driverId', '$requestId')");
     
     $result2 = mysql_query("INSERT INTO `taxiapp`.`driver_inbox` (`driver_id`, `message`) VALUES ('$driverId', 'mdssada')");
+    
+    echo $contact_no;
 }
 
 ?>
